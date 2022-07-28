b2ddcff419c42436237b9ed701451afcb6ca082c
Kaleb Franken <skywrithin@gmail.com>
2022-07-27 18:18:31 -0600

    LeetCode 49. Group Anagrams. (Faster than 98% of TS solutions)

--- src/Arrays & Hashing/group-anagrams.ts
+++ src/Arrays & Hashing/group-anagrams.ts
@@ -3,7 +3,6 @@ function groupAnagrams(strs: string[]): string[][] {
     const answerArray = new Array();
     
     for (let string of strs){
-        let strArray:string[];
         let mapKey = [...string].sort().join('');
         let mapValue =  anagramMap.get(mapKey) || null
         mapValue? anagramMap.set(mapKey, [...mapValue , string]) : anagramMap.set(mapKey, [string]);
