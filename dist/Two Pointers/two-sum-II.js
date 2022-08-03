981f8c7dc98258949c807a35d0945f761daa42b6
Kaleb Franken <skywrithin@gmail.com>
2022-08-03 16:02:04 -0600

    commit

--- dist/Two Pointers/two-sum-II.js
+++ dist/Two Pointers/two-sum-II.js
@@ -1,2 +1,13 @@
 "use strict";
+function twoSum(numbers, target) {
+    debugger;
+    let j = numbers.length;
+    for (let i = 1; i < j; i++) {
+        while (numbers[i - 1] + numbers[j - 1] > target) {
+            j--;
+        }
+        if (numbers[i - 1] + numbers[j - 1] === target)
+            return [i, j];
+    }
+}
 //# sourceMappingURL=two-sum-II.js.map
--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,3 @@
 "use strict";
+console.log('connected');
 //# sourceMappingURL=app.js.map
