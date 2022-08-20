79ea439b9b19cc116a8fc4b5418fc5ef07e9c5e6
Kaleb Franken <skywrithin@gmail.com>
2022-08-20 17:28:39 -0600

    238. Product of Array Except Self

---
+++ src/Arrays & Hashing/product-of-array-except-self.ts
@@ -0,0 +1,15 @@
+function productExceptSelf(nums: number[]): number[] {
+    let answer: number[] = new Array(nums.length).fill(1);
+    let phelp: number = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        phelp = phelp * nums[i - 1];
+        answer[i] = phelp;
+    }
+    phelp = 1;
+    for (let j = nums.length - 2; j >= 0; j--) {
+        phelp = phelp * nums[j + 1];
+        answer[j] = answer[j] * phelp;
+    }
+    return [1];
+}
