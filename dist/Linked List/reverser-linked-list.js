639e350bf600fac1664fa9f30ce22189ddecad6e
Kaleb Franken <skywrithin@gmail.com>
2022-09-09 02:49:12 -0600

    changes

---
+++ dist/Linked List/reverser-linked-list.js
@@ -0,0 +1,22 @@
+"use strict";
+class ListNode {
+    constructor(val, next) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+function reverseList(head) {
+    if (head === null) {
+        return head;
+    }
+    let prev = null;
+    let current = head;
+    while (current) {
+        let next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+//# sourceMappingURL=reverser-linked-list.js.map
