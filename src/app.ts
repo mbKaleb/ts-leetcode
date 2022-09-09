b01608eb4400f7d8d3b08df3603c016bd15c778b
Kaleb Franken <skywrithin@gmail.com>
2022-09-09 09:47:46 -0600

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
 console.log('connected');
+
+
+function maxDepth(root: TreeNode | null): number {
+    if (!root) return null
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
