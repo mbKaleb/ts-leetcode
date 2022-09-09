db906f3b22f1db31a72e0d7a0f2bceba1fff2950
Kaleb Franken <skywrithin@gmail.com>
2022-09-09 06:50:07 -0600

    206. Reverse Linked List

--- src/Linked List/reverse-linked-list.ts
+++ src/Linked List/reverse-linked-list.ts
@@ -1,12 +1,3 @@
-class ListNode {
-    val: number;
-    next: ListNode | null;
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = val === undefined ? 0 : val;
-        this.next = next === undefined ? null : next;
-    }
-}
-
 function reverseList(head: ListNode | null): any {
     if (head === null) {
         return head;
