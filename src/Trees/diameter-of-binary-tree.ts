408f8f0394521fe0c976fe6b4b158b2962e5b8b3
Kaleb Franken <skywrithin@gmail.com>
2022-09-09 10:29:13 -0600

    543. Diameter of Binary Tree

---
+++ src/Trees/diameter-of-binary-tree.ts
@@ -0,0 +1,14 @@
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    let res = 0;
+    function dfs(root: TreeNode) {
+        if (!root) {
+            return -1;
+        }
+        let left: number = dfs(root.left);
+        let right: number = dfs(root.right);
+        res = Math.max(res, 2 + left + right);
+        return 1 + Math.max(left, right);
+    }
+    dfs(root);
+    return res;
+}
